ec1fd706605c0acf2df6d6c5041dbc40f4cade38
itanglishdev <chrishethertonweb@gmail.com>
2022-11-29 14:19:42 +0100

    done 1 to 4

--- solution.js
+++ solution.js
@@ -8,3 +8,23 @@
 
 // module.exports.sum = (a,b,c,d) => a + b + c + d
 // console.log(module.exports)
+
+// console.log('the precess is ', process.argv);
+let total = 0
+
+if (process.argv[2] !== 'sum') {
+    return console.log('Sorry, cannot calculate');
+} for (let i = 3;  i < process.argv.length; i++ ) {
+    if (typeof process.argv[i] !== 'number') {
+        return console.log(`Sorry ${process.argv[i]} is not a number`)
+    } function sum() {
+        for (let i = 3; i < process.argv.length; i++ ) {
+            total += Number(process.argv[i])
+        }
+    }
+}
+
+
+sum()
+
+console.log(total)
