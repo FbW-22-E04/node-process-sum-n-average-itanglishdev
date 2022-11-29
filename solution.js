27be425b40f0cc681969e0bc0c5f5c7a744039a1
itanglishdev <chrishethertonweb@gmail.com>
2022-11-29 14:00:42 +0100

    reset all

---
+++ solution.js
@@ -0,0 +1,10 @@
+// const sum = (a,b,c,d) => (a + b + c + d)
+
+// function sum(a,b,c,d) {
+//     a + b + c + d
+// }
+
+// module.exports = {sum}
+
+// module.exports.sum = (a,b,c,d) => a + b + c + d
+// console.log(module.exports)
